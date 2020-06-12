4f2db6d9186e75e69472d7b77b29b9acbf8ddbf1
ahamideh <ahamideh18@hotmail.com>
2020-06-12 14:33:49 -0400

    Updates

---
+++ precache-manifest.138ba2dff43797fdb68f2e6239d56207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec35f1313d72a54d27a0d723ee351faf",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "36b89e8751fcc8d5b6bf",
+    "url": "/MonstersRolodex/static/css/main.5af0f3da.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/MonstersRolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b89e8751fcc8d5b6bf",
+    "url": "/MonstersRolodex/static/js/main.a58d453b.chunk.js"
+  },
+  {
+    "revision": "6e7c5abc2ba8b11e5aa8",
+    "url": "/MonstersRolodex/static/js/runtime-main.840ef2d2.js"
+  }
+]);
